db605cbb45d883b7b3dcf1a7744f5879d99ba402
Andrii Andreiev <andrii.andreiev@avast.com>
2022-12-23 20:21:59 +0200

    Initial Commit

---
+++ TuneIn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TuneIn
+//
+//  Created by Andrii Andreiev on 23.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TuneIn/TuneInApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneInApp.swift
+//  TuneIn
+//
+//  Created by Andrii Andreiev on 23.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TuneInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
